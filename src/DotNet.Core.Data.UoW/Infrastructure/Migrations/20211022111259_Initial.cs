de4ee5d1d7cdda812570258f6ba59bdb8ef11558
Sanjyot Agureddy <sanjyot.agureddy4@gmail.com>
2021-10-22 16:44:00 +0530

    Completed code for UoW

---
+++ src/DotNet.Core.Data.UoW/Infrastructure/Migrations/20211022111259_Initial.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DotNet.Core.Data.UoW.Infrastructure.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Product",
+                columns: new[] { "Id", "Description", "Name", "Price" },
+                values: new object[] { 1, "A description", "Product A", 49.990000000000002 });
+
+            migrationBuilder.InsertData(
+                table: "Product",
+                columns: new[] { "Id", "Description", "Name", "Price" },
+                values: new object[] { 2, "B description", "Product B", 29.989999999999998 });
+
+            migrationBuilder.InsertData(
+                table: "Product",
+                columns: new[] { "Id", "Description", "Name", "Price" },
+                values: new object[] { 3, "C description", "Product C", 89.989999999999995 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product");
+        }
+    }
+}
